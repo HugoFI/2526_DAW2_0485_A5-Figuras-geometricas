2b67145142bf198fae6a3286a2b91ebe09632336
HugoFI <100006119.joan23@fje.edu>
2025-10-15 16:09:12 +0200

    Revert "update resultados e introducir_lados"
    This reverts commit 20f921f8fd89a96d9a3ac8c20f5cc3e769aceccb.

--- resultado.php
+++ resultado.php
@@ -78,11 +78,9 @@ $lado2 = isset($_POST['lado2']) ? floatval($_POST['lado2']) : null;
             }
             ?>
             <div class="d-grid mt-4">
-                <form action="introducir_lados.php" method="get" class="mb-2">
-                    <input type="hidden" name="figura" value="<?php echo $figura; ?>">
-                    <button type="submit" class="btn btn-secondary btn-lg">Editar lados</button>
-                    <a href="./index.php" class="btn btn-primary btn-lg">Volver al inicio</a>
-                </form>
+                <a href="./introducir_lados.php" class="btn btn-secondary" name="figura" value="<?php echo $figura; ?>">Editar lados</a>
+                <br>
+                <a href="./index.php" class="btn btn-primary btn-lg">Volver al inicio</a>
             </div>
         </div>
     </div>
